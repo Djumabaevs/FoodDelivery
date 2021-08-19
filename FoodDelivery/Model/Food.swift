5cf53b3ac883ea4f3202bdf7a0c0827586f3aac4
BakytDzhumabev <djumabaevb@gmail.com>
2021-08-20 01:44:28 +0600

    food : washable, codable, identifiable

--- FoodDelivery/Model/Food.swift
+++ FoodDelivery/Model/Food.swift
@@ -5,4 +5,14 @@
 //  Created by Bakyt Dzhumabaev on 20/8/21.
 //
 
-import Foundation
+import SwiftUI
+
+struct Food: Hashable, Codable, Identifiable {
+    
+    var id: String
+    var name: String
+    var image: String
+    var kitchen: String
+    var description: String
+        
+}
